2759439913793ea1c853e47420a443c9adf6e74a
Ben Cook <b@bencook.ca>
2015-11-11 07:25:19 -0800

    Simplify hash-word because it doesn't actually need to be constrained to indexes-of-words

--- src/words/lib.clj
+++ src/words/lib.clj
@@ -18,14 +18,10 @@
          (BigInteger. 1))))
 
 (defn hash-word
-  "Return the index onto `words` obtained by hashing `word`."
   [word]
-  (let [hash (str (md5-base10 word))
-        len (min (count hash)
-                 (-> (str words-count) count inc))]
-    (-> hash
-        (subs 0 len)
-        Long/parseLong)))
+  (-> (md5-base10 word)
+      (mod Long/MAX_VALUE)
+      long))
 
 (defn get-rng [seed-word]
   (java.util.Random. (hash-word seed-word)))
